5b5da2b1a7b47137866d6e10b55473ddf24892ff
Rahul Goswami <52671942+RahulGoswami2000@users.noreply.github.com>
2024-03-11 17:30:36 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.use(express.json());
 
 let users = [];
 
-app.post('/users', (req, res) => {
+app.post('/add', (req, res) => {
      const { name, email } = req.body;
      if (!name || !email) {
          return res.status(400).json({ error: "Name and email are required" });
@@ -14,36 +14,33 @@ app.post('/users', (req, res) => {
      res.status(201).json({ message: "User added", success: true });
  });
  
- // Route to get all users
+
  app.get('/users', (req, res) => {
      res.status(200).json({ message: "Users retrieved", success: true, users: users });
  });
 
- app.put('/users/:id', (req, res) => {
+ app.put('/update/:id', (req, res) => {
      const userId = parseInt(req.params.id);
      const { name, email } = req.body;
      const userIndex = users.findIndex(user => user.id === userId);
      if (userIndex === -1) {
          return res.status(404).json({ error: "User not found" });
      }
- 
-     // Update user details
+
      users[userIndex].name = name;
      users[userIndex].email = email;
  
      res.status(200).json({ message: "User updated", success: true });
  });
 
- app.get('/users/:id', (req, res) => {
-     const userId = parseInt(req.params.id); // Convert URL parameter to integer
- 
-     // Find the user by ID
+ app.get('/user/:id', (req, res) => {
+     const userId = parseInt(req.params.id);
      const user = users.find(user => user.id === userId);
      if (!user) {
          return res.status(404).json({ error: "User not found" });
      }
  
-     res.status(200).json({ message: "User retrieved", success: true, user: user });
+     res.status(200).json({ success: true, user: user });
  });
  
 
